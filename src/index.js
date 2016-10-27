dfb96856aa19c9f04e1515f9752177d289e35fa2
Kamil Biedrzycki <kamil.biedrzycki@airhelp.com>
2016-10-27 19:24:45 +0200

    Init with create react app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// choose proper lesson to start hacking!
+// to choose lesson simply uncomment line and save file
+
+// require('./exercises/ex-01');
+// require('./exercises/ex-02');
+// require('./exercises/ex-03');
+// require('./exercises/ex-04');
+// require('./exercises/ex-05');
+// require('./exercises/ex-06');
+// require('./exercises/ex-07');
+// require('./exercises/ex-08');
